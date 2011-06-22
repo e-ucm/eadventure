beb5445f7f8a3d41b9bc49b14d8913703698e738
emarchiori <emarchiori@gmail.com>
2011-06-23 00:25:05 +0200

    Slight inventory improvements

--- core/engine-desktop/impl/src/main/java/es/eucm/eadventure/engine/core/platform/impl/extra/DesktopBasicInventoryGO.java
+++ core/engine-desktop/impl/src/main/java/es/eucm/eadventure/engine/core/platform/impl/extra/DesktopBasicInventoryGO.java
@@ -26,34 +26,43 @@ import es.eucm.eadventure.engine.core.gameobjects.impl.inventory.BasicInventoryG
 import es.eucm.eadventure.engine.core.gameobjects.impl.sceneelements.ActorReferenceGOImpl;
 
 public class DesktopBasicInventoryGO extends BasicInventoryGO {
-
-	private EAdBasicSceneElement bottomPart;
-
-	private EAdBasicSceneElement topPart;
 	
-	private EAdBasicSceneElement centerPart;
-
-	private EAdComplexSceneElement inventory;
-	
-	private Map<EAdActor, EAdActorReferenceImpl> includedActors;
-	
+	/**
+	 * The height of the top and bottom sensors
+	 */
 	private static int SENSE_HEIGHT = 40;
 	
+	/**
+	 * The inventory height
+	 */
 	private static int INVENTORY_HEIGHT = 100;
+
+	/**
+	 * bottom sensor which activates the inventory
+	 */
+	private EAdBasicSceneElement bottomSensor;
+
+	/**
+	 * top sensor which activates the inventory
+	 */
+	private EAdBasicSceneElement topSensor;
+	
+	/**
+	 * center sensor, which hides the inventory
+	 */
+	private EAdBasicSceneElement centerSensor;
+
+	/**
+	 * the actual inventory
+	 */
+	private EAdComplexSceneElement inventory;
+	
+	/**
+	 * the map of actors and actor references in the inventory
+	 */
+	private Map<EAdActor, EAdActorReferenceImpl> includedActors;
 	
 	public DesktopBasicInventoryGO() {
-		bottomPart = new EAdBasicSceneElement("bottomInventory");
-		bottomPart.setDraggabe(EmptyCondition.FALSE_EMPTY_CONDITION);
-		RectangleShape rect = new RectangleShape(800, SENSE_HEIGHT + 2, EAdBorderedColor.TRANSPARENT);
-		
-		bottomPart.getResources().addAsset(bottomPart.getInitialBundle(), EAdBasicSceneElement.appearance, rect);
-		bottomPart.setPosition(new EAdPosition(Corner.BOTTOM_LEFT, 0, 601));
-		
-		topPart = new EAdBasicSceneElement("topInventory");
-		topPart.setDraggabe(EmptyCondition.FALSE_EMPTY_CONDITION);
-		topPart.getResources().addAsset(topPart.getInitialBundle(), EAdBasicSceneElement.appearance, rect);
-		topPart.setPosition(new EAdPosition(Corner.TOP_LEFT, 0, -1));
-		
 		inventory = new EAdComplexSceneElement("inventory");
 		inventory.setDraggabe(EmptyCondition.FALSE_EMPTY_CONDITION);
 		inventory.setPosition(new EAdPosition(Corner.BOTTOM_LEFT, 0, 700));
@@ -61,62 +70,84 @@ public class DesktopBasicInventoryGO extends BasicInventoryGO {
 		RectangleShape rect2 = new RectangleShape(800, INVENTORY_HEIGHT, EAdBorderedColor.BLACK_ON_WHITE);
 		inventory.getResources().addAsset(inventory.getInitialBundle(), EAdBasicSceneElement.appearance, rect2);
 
-		centerPart = new EAdBasicSceneElement("centerPart");
-		centerPart.setDraggabe(EmptyCondition.FALSE_EMPTY_CONDITION);
-		centerPart.getResources().addAsset(centerPart.getInitialBundle(), EAdBasicSceneElement.appearance,  new RectangleShape(800, 600, EAdBorderedColor.TRANSPARENT));
-		centerPart.setPosition(new EAdPosition(Corner.TOP_LEFT, 0, 0));
+		RectangleShape rect = new RectangleShape(800, SENSE_HEIGHT + 2, EAdBorderedColor.TRANSPARENT);
+
+		bottomSensor = createSensorPart(rect, 601, 600, 700);
 		
+		topSensor = createSensorPart(rect, -1, 100, 0);
 		
-		EAdMoveSceneElement e = new EAdMoveSceneElement("moveInventoryToBottom", inventory, 0, 700, MovementSpeed.INSTANT);
-		bottomPart.addBehavior(EAdMouseEventImpl.MOUSE_ENTERED, e);
-		e = new EAdMoveSceneElement("showInventoryBottom", inventory, 0, 600, MovementSpeed.FAST);
-		bottomPart.addBehavior(EAdMouseEventImpl.MOUSE_ENTERED, e);
+		createCenterPart();
+		
+		includedActors = new HashMap<EAdActor, EAdActorReferenceImpl>();
+	}
+	
+	/**
+	 * Create the center sensor part
+	 */
+	private void createCenterPart() {
+		centerSensor = new EAdBasicSceneElement("centerPart");
+		centerSensor.setDraggabe(EmptyCondition.FALSE_EMPTY_CONDITION);
+		centerSensor.getResources().addAsset(centerSensor.getInitialBundle(), EAdBasicSceneElement.appearance,  new RectangleShape(800, 600, EAdBorderedColor.TRANSPARENT));
+		centerSensor.setPosition(new EAdPosition(Corner.TOP_LEFT, 0, 0));
 		
 		EAdMoveSceneElement e2 = new EAdMoveSceneElement("hideInventoryBottom", inventory, 0, 700, MovementSpeed.NORMAL);
 		e2.setCondition(new VarValCondition(inventory.positionYVar(), 350, Operator.GREATER));
-//		bottomPart.addBehavior(EAdMouseEventImpl.MOUSE_EXITED, e2);
-		centerPart.addBehavior(EAdMouseEventImpl.MOUSE_MOVED, e2);
+		centerSensor.addBehavior(EAdMouseEventImpl.MOUSE_MOVED, e2);
 
-		e = new EAdMoveSceneElement("moveInventoryToTop", inventory, 0, 0, MovementSpeed.INSTANT);
-		topPart.addBehavior(EAdMouseEventImpl.MOUSE_ENTERED, e);
-		e = new EAdMoveSceneElement("showInventoryTop", inventory, 0, 100, MovementSpeed.FAST);
-		topPart.addBehavior(EAdMouseEventImpl.MOUSE_ENTERED, e);
-
-		
 		e2 = new EAdMoveSceneElement("hideInventoryTop", inventory, 0, 0, MovementSpeed.NORMAL);
 		e2.setCondition(new VarValCondition(inventory.positionYVar(), 350, Operator.LESS));
-//		topPart.addBehavior(EAdMouseEventImpl.MOUSE_EXITED, e2);
-		centerPart.addBehavior(EAdMouseEventImpl.MOUSE_MOVED, e2);
+		centerSensor.addBehavior(EAdMouseEventImpl.MOUSE_MOVED, e2);
+	}
+	
+	/**
+	 * Create sensor part
+	 * 
+	 * @param rect The rectangle resource
+	 * @param sensorPos The position of the sensor part
+	 * @param inventoryPos The position of the inventory
+	 * @param hidePos The position of the inventory when hidden
+	 * @return The sensor part
+	 */
+	private EAdBasicSceneElement createSensorPart(RectangleShape rect, int sensorPos, int inventoryPos, int hidePos) {
+		EAdBasicSceneElement part = new EAdBasicSceneElement("inventorySensor");
+		part.setDraggabe(EmptyCondition.FALSE_EMPTY_CONDITION);
+		part.getResources().addAsset(part.getInitialBundle(), EAdBasicSceneElement.appearance, rect);
+		part.setPosition(new EAdPosition(Corner.BOTTOM_LEFT, 0, sensorPos));
 		
-		includedActors = new HashMap<EAdActor, EAdActorReferenceImpl>();
-		
+		EAdMoveSceneElement e = new EAdMoveSceneElement("moveInventory", inventory, 0, hidePos, MovementSpeed.INSTANT);
+		part.addBehavior(EAdMouseEventImpl.MOUSE_ENTERED, e);
+		e = new EAdMoveSceneElement("showInventory", inventory, 0, inventoryPos, MovementSpeed.FAST);
+		part.addBehavior(EAdMouseEventImpl.MOUSE_ENTERED, e);
+		return part;
 	}
 	
 	@Override
 	public void doLayout(int offsetX, int offsetY) {
-		gui.addElement(gameObjectFactory.get(centerPart), 0, 0);
+		gui.addElement(gameObjectFactory.get(centerSensor), 0, 0);
 		gui.addElement(gameObjectFactory.get(inventory), 0, 0);
-		gui.addElement(gameObjectFactory.get(bottomPart), 0, 0);
-		gui.addElement(gameObjectFactory.get(topPart), 0, 0);
+		gui.addElement(gameObjectFactory.get(bottomSensor), 0, 0);
+		gui.addElement(gameObjectFactory.get(topSensor), 0, 0);
 	}
 	
 	@Override
 	public void update(GameState gameState) {
 		super.update(gameState);
-		gameObjectFactory.get(bottomPart).update(gameState);
-		gameObjectFactory.get(topPart).update(gameState);
+		gameObjectFactory.get(bottomSensor).update(gameState);
+		gameObjectFactory.get(topSensor).update(gameState);
 		gameObjectFactory.get(inventory).update(gameState);
 		
 		List<EAdActor> removedActors = new ArrayList<EAdActor>();
 		addNewActors();
+		int cont = 0;
 		for (EAdActor actor : includedActors.keySet()) {
 			if (!gameState.getInventoryActors().contains(actor))
 				removedActors.add(actor);
 			else {
 				EAdActorReferenceImpl ref = includedActors.get(actor);
-				ref.setPosition(new EAdPosition(EAdPosition.Corner.CENTER, 60, 60));
+				valueMap.setValue(ref.positionXVar(), cont * (10 + INVENTORY_HEIGHT) + INVENTORY_HEIGHT / 2);
 				
 				//TODO position actor
+				cont++;
 			}
 		}
 		removeOldActors(removedActors);
@@ -131,6 +162,7 @@ public class DesktopBasicInventoryGO extends BasicInventoryGO {
 				EAdActorReferenceImpl ref = new EAdActorReferenceImpl(actor);
 				EAdActorActionsEffect showActions = new EAdActorActionsEffect( ref.getId()+ "_showActions", ref);
 				ref.getBehavior().addBehavior(EAdMouseEventImpl.MOUSE_RIGHT_CLICK, showActions);
+				ref.setPosition(new EAdPosition(EAdPosition.Corner.CENTER, INVENTORY_HEIGHT / 2 + 10, INVENTORY_HEIGHT / 2));
 				((ActorReferenceGOImpl) gameObjectFactory.get(ref)).setInventoryReference(true);
 				includedActors.put(actor, ref);
 				inventory.getComponents().add(ref);
