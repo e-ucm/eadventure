540d4ab3fddae5f2894c24c6bc0dca0d7fda2763
anserran <angelserranolaguna@gmail.com>
2011-07-11 18:46:54 +0200

    Added effect to move active element when click

---
+++ core/engine-core/impl/src/main/java/es/eucm/eadventure/engine/core/gameobjects/impl/effects/MoveActiveElementGO.java
@@ -0,0 +1,61 @@
+package es.eucm.eadventure.engine.core.gameobjects.impl.effects;
+
+import es.eucm.eadventure.common.model.EAdList;
+import es.eucm.eadventure.common.model.effects.impl.EAdMoveActiveElement;
+import es.eucm.eadventure.common.model.effects.impl.sceneelements.EAdMoveSceneElement;
+import es.eucm.eadventure.common.model.effects.impl.sceneelements.EAdMoveSceneElement.MovementSpeed;
+import es.eucm.eadventure.common.model.elements.EAdScene;
+import es.eucm.eadventure.common.model.params.EAdPosition;
+import es.eucm.eadventure.common.resources.StringHandler;
+import es.eucm.eadventure.engine.core.GameState;
+import es.eucm.eadventure.engine.core.ValueMap;
+import es.eucm.eadventure.engine.core.gameobjects.GameObjectFactory;
+import es.eucm.eadventure.engine.core.guiactions.MouseAction;
+import es.eucm.eadventure.engine.core.platform.AssetHandler;
+import es.eucm.eadventure.engine.core.platform.GUI;
+import es.eucm.eadventure.engine.core.platform.PlatformConfiguration;
+
+public class MoveActiveElementGO extends AbstractEffectGO<EAdMoveActiveElement> {
+
+	public MoveActiveElementGO(AssetHandler assetHandler,
+			StringHandler stringHandler, GameObjectFactory gameObjectFactory,
+			GUI gui, GameState gameState, ValueMap valueMap,
+			PlatformConfiguration platformConfiguration) {
+		super(assetHandler, stringHandler, gameObjectFactory, gui, gameState,
+				valueMap, platformConfiguration);
+	}
+
+	@Override
+	public void initilize() {
+		super.initilize();
+		Object object = gameState.getScene().getElement();
+		if (object instanceof EAdScene && action instanceof MouseAction) {
+			int x = ((MouseAction) action).getVirtualX();
+			int y = ((MouseAction) action).getVirtualY();
+			EAdScene scene = (EAdScene) object;
+			if (scene.getTrajectoryGenerator() != null) {
+				EAdList<EAdPosition> trajectory = scene
+						.getTrajectoryGenerator().getTrajectory(
+								gameState.getActiveElement().getPosition(), x,
+								y);
+				for (EAdPosition p : trajectory) {
+					EAdMoveSceneElement effect = new EAdMoveSceneElement(
+							"trajectory", gameState.getActiveElement(),
+							p.getX(), p.getY(), MovementSpeed.NORMAL);
+					gameState.addEffect(effect);
+				}
+			}
+		}
+	}
+
+	@Override
+	public boolean isVisualEffect() {
+		return false;
+	}
+
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
+
+}
