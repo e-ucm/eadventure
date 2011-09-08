57c168d3887b65ca03b1cc900e5444419ced20a0
anserran <angelserranolaguna@gmail.com>
2011-09-08 16:54:11 +0200

    Updated XML reader/writer

---
+++ core/common/impl/src/main/java/es/eucm/eadventure/common/impl/writer/ParamDOMWriter.java
@@ -0,0 +1,30 @@
+package es.eucm.eadventure.common.impl.writer;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+
+import es.eucm.eadventure.common.params.EAdParam;
+
+public class ParamDOMWriter extends DOMWriter<EAdParam>{
+	
+	public static final String PREFIX = "param";
+
+	@Override
+	public Element buildNode(EAdParam data) {
+		try {
+			initilizeDOMWriter();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		node = doc.createElement("param");
+		String value = paramsMap.get(data);
+		if ( value == null ){
+			value = data.toStringData();
+			paramsMap.put(value, "" + paramsMap.keySet().size());
+		}
+		node.setTextContent(value);
+		return node;
+	}
+
+}
