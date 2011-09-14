e31062d8f5ebf290588f424dd5793fcfd0e489a9
emarchiori <emarchiori@gmail.com>
2011-09-14 19:16:58 +0200

    Fixed bug with VLC video renderer.

--- core/engine-desktop/impl/src/main/java/es/eucm/eadventure/engine/core/platform/impl/specialassetrenderers/VLCDesktopVideoRenderer.java
+++ core/engine-desktop/impl/src/main/java/es/eucm/eadventure/engine/core/platform/impl/specialassetrenderers/VLCDesktopVideoRenderer.java
@@ -50,33 +50,63 @@ import com.google.inject.Inject;
 import es.eucm.eadventure.common.resources.assets.multimedia.Video;
 import es.eucm.eadventure.engine.core.platform.AssetHandler;
 import es.eucm.eadventure.engine.core.platform.SpecialAssetRenderer;
+import es.eucm.eadventure.engine.core.platform.impl.specialassetrenderers.extra.VLCMediaPlayerEventListener;
 import es.eucm.eadventure.engine.core.platform.impl.specialassetrenderers.extra.WinRegistry;
 
-import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
-import uk.co.caprica.vlcj.player.MediaPlayer;
-import uk.co.caprica.vlcj.player.MediaPlayerEventListener;
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 import uk.co.caprica.vlcj.player.embedded.videosurface.CanvasVideoSurface;
 
+/**
+ * <p>Video renderer for desktop (and applets) using vlcj library {@link http://code.google.com/p/vlcj/}</p>
+ * 
+ *
+ */
 public class VLCDesktopVideoRenderer implements SpecialAssetRenderer<Video, Component> {
 	
+	/**
+	 * Logger
+	 */
 	private static Logger logger = Logger.getLogger("VLCDesktopVideoRenderer");
 
+	/**
+	 * The vlcj media player (controls, etc.)
+	 */
 	private EmbeddedMediaPlayer mediaPlayer;
 
+	/**
+	 * The vlcj surface for the video
+	 */
 	private CanvasVideoSurface videoSurface;
 	
+	/**
+	 * True if finished
+	 */
 	protected boolean finished;
 	
+	/**
+	 * True if started
+	 */
 	protected boolean started;
 
+	/**
+	 * The vlcj media player factory
+	 */
 	private static MediaPlayerFactory mediaPlayerFactory;
 	
+	/**
+	 * Used to configure vlc when necessary
+	 */
 	private static String vlcOptions = "";
 	
+	/**
+	 * The path of the video file
+	 */
 	private String path;
 	
+	/**
+	 * The eAd asset handler
+	 */
 	private AssetHandler assetHandler;
 
 	@Inject
@@ -100,93 +130,7 @@ public class VLCDesktopVideoRenderer implements SpecialAssetRenderer<Video, Comp
 		videoSurface = mediaPlayerFactory.newVideoSurface(canvas);
 		mediaPlayer.setVideoSurface(videoSurface);
 		
-		mediaPlayer.addMediaPlayerEventListener(new MediaPlayerEventListener() {
-
-			@Override
-			public void backward(MediaPlayer arg0) { }
-
-			@Override
-			public void buffering(MediaPlayer arg0) { }
-
-			@Override
-			public void endOfSubItems(MediaPlayer arg0) { }
-
-			@Override
-			public void error(MediaPlayer arg0) { }
-
-			@Override
-			public void finished(MediaPlayer arg0) {
-				finished = true;
-				started = false;
-			}
-
-			@Override
-			public void forward(MediaPlayer arg0) { }
-
-			@Override
-			public void lengthChanged(MediaPlayer arg0, long arg1) { }
-
-			@Override
-			public void mediaChanged(MediaPlayer arg0) { }
-
-			@Override
-			public void mediaDurationChanged(MediaPlayer arg0, long arg1) { }
-
-			@Override
-			public void mediaFreed(MediaPlayer arg0) { }
-
-			@Override
-			public void mediaMetaChanged(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void mediaParsedChanged(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void mediaStateChanged(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void mediaSubItemAdded(MediaPlayer arg0, libvlc_media_t arg1) { }
-
-			@Override
-			public void newMedia(MediaPlayer arg0) { }
-
-			@Override
-			public void opening(MediaPlayer arg0) { }
-
-			@Override
-			public void pausableChanged(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void paused(MediaPlayer arg0) { }
-
-			@Override
-			public void playing(MediaPlayer arg0) { }
-
-			@Override
-			public void positionChanged(MediaPlayer arg0, float arg1) { }
-
-			@Override
-			public void seekableChanged(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void snapshotTaken(MediaPlayer arg0, String arg1) { }
-
-			@Override
-			public void stopped(MediaPlayer arg0) { }
-
-			@Override
-			public void subItemFinished(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void subItemPlayed(MediaPlayer arg0, int arg1) { }
-
-			@Override
-			public void timeChanged(MediaPlayer arg0, long arg1) { }
-
-			@Override
-			public void titleChanged(MediaPlayer arg0, int arg1) { }
-			
-		});
+		mediaPlayer.addMediaPlayerEventListener(new VLCMediaPlayerEventListener(this));
 		
 		path = asset.getURI().getPath();
 		if (assetHandler != null)
@@ -197,7 +141,10 @@ public class VLCDesktopVideoRenderer implements SpecialAssetRenderer<Video, Comp
 		return canvas;
 	}
 	
-	public static void initilizeVariables() {
+	/**
+	 * Initialize system and system dependent variables for vlcj.
+	 */
+	private void initilizeVariables() {
 		String os = System.getProperty("os.name").toLowerCase();
 		if (os.contains("win")) {
 			String temp = null;
@@ -246,13 +193,31 @@ public class VLCDesktopVideoRenderer implements SpecialAssetRenderer<Video, Comp
 
 	@Override
 	public boolean start() {
-		if (!started) {
+		if (!started && mediaPlayer != null) {
 			String [] mediaOptions ={};
 			mediaPlayer.playMedia(path, mediaOptions);
 			started = true;
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * Set the finished flag
+	 * 
+	 * @param b The new value for finished
+	 */
+	public void setFinished(boolean b) {
+		this.finished = b;
+	}
+
+	/**
+	 * Set the started flag
+	 * 
+	 * @param b The new value for started
+	 */
+	public void setStarted(boolean b) {
+		this.started = b;
 	}
 	
 }
