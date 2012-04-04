bb74a30f4d6e414fb8c4912d832f64a0f026d3df
anserran <angelserranolaguna@gmail.com>
2012-04-04 16:26:10 +0200

    Added missing licenses.

---
+++ core/model/src/main/java/ead/common/resources/assets/drawable/basics/shapes/LineShape.java
@@ -0,0 +1,56 @@
+/**
+ * eAdventure (formerly <e-Adventure> and <e-Game>) is a research project of the
+ *    <e-UCM> research group.
+ *
+ *    Copyright 2005-2010 <e-UCM> research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    <e-UCM> is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure, version 2.0
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ead.common.resources.assets.drawable.basics.shapes;
+
+public class LineShape extends BezierShape {
+
+	public LineShape(int x1, int y1, int x2, int y2, int width) {
+		int vy = x2 - x1;
+		int vx = y2 - y1;
+		float module = (float) Math.sqrt(vx * vx + vy * vy);
+		vy = (int) (((float) vy / module) * width);
+		vx = (int) (((float) vx / module) * width);
+
+		moveTo(x1 - vx, y1 - vy);
+		lineTo(x1 + vx, y1 + vy);
+		lineTo(x2 + vx, y2 + vy);
+		lineTo(x2 - vx, y2 - vy);
+		setClosed(true);
+	}
+
+}
