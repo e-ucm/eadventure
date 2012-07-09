07a982a85c897b003792216decd6fc6a66d6703e
manuel freire <manuel.freire@gmail.com>
2012-07-09 17:46:52 +0200

    editor SplashScreenImpl.java no longer flickers

--- core/editor-core/src/main/java/ead/editor/view/impl/SplashScreenImpl.java
+++ core/editor-core/src/main/java/ead/editor/view/impl/SplashScreenImpl.java
@@ -51,124 +51,136 @@ import java.awt.event.ActionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 /**
  * Default implementation of the eAdventure editor splash screen.
  */
 public class SplashScreenImpl implements SplashScreen {
 
-	/**
-	 * Logger
-	 */
-	private static final Logger logger = LoggerFactory.getLogger("SplashScreenImpl");
+    /**
+     * Logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger("SplashScreenImpl");
+    /**
+     * The splash screen dialog
+     */
+    protected SplashScreenDialog splashScreenDialog;
+    /**
+     * The background image of the dialog
+     */
+    protected Image image;
+    /**
+     * The time at which the splash screen appeared
+     */
+    private long startTime;
+    /**
+     * The minimum time to display the splash screen
+     */
+    private static final long MIN_TIME = 4000;
 
-	/**
-	 * The splash screen dialog
-	 */
-	protected SplashScreenDialog splashScreenDialog;
-
-	/**
-	 * The background image of the dialog
-	 */
-	protected Image image;
-
-	/**
-	 * The time at which the splash screen appeared
-	 */
-	private long startTime;
-
-	/**
-	 * The minimum time to display the splash screen
-	 */
-	private static final long MIN_TIME = 4000;
-
-	@Override
-	public void show() {
-		logger.info("Showing Splash-screen");
+    @Override
+    public void show() {
+        logger.info("Showing Splash-screen");
 
         image = Resource.loadImage(R.Drawable.SplashScreenLogo_png);
 
-		SwingUtilities.doInEDTNow(new Runnable() {
-			@Override
-			public void run() {
-				splashScreenDialog = new SplashScreenDialog();
-				splashScreenDialog.setUndecorated(true);
+        SwingUtilities.doInEDTNow(new Runnable() {
 
-		        int width = image.getWidth( null );
-		        int height = image.getHeight( null );
-		        splashScreenDialog.setSize( width, height );
-		        splashScreenDialog.setResizable( false );
+            @Override
+            public void run() {
+                splashScreenDialog = new SplashScreenDialog();
+                splashScreenDialog.setUndecorated(true);
 
-                Dimension screenSize = Toolkit.getDefaultToolkit( ).getScreenSize( );
-		        double screenWidth = screenSize.getWidth( );
-		        double screenHeight = screenSize.getHeight( );
-		        int locX = Math.round( ( (int) screenWidth - width ) / 2.0f );
-		        int locY = Math.round( ( (int) screenHeight - height ) / 2.0f );
-		        splashScreenDialog.setLocation( locX, locY );
-		        splashScreenDialog.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) );
+                int width = image.getWidth(null);
+                int height = image.getHeight(null);
+                splashScreenDialog.setSize(width, height);
+                splashScreenDialog.setResizable(false);
 
-		        splashScreenDialog.setVisible(true);
-			}
-		});
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                double screenWidth = screenSize.getWidth();
+                double screenHeight = screenSize.getHeight();
+                int locX = Math.round(((int) screenWidth - width) / 2.0f);
+                int locY = Math.round(((int) screenHeight - height) / 2.0f);
+                splashScreenDialog.setLocation(locX, locY);
+                splashScreenDialog.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 
-		startTime = System.currentTimeMillis();
-	}
+                splashScreenDialog.setVisible(true);
+            }
+        });
 
-	@Override
-	public void hide() {
-		logger.info("Hiding Splash-screen");
+        startTime = System.currentTimeMillis();
+    }
 
-		if (System.currentTimeMillis() - startTime < MIN_TIME) {
-			try {
-				Thread.sleep(MIN_TIME - (System.currentTimeMillis() - startTime));
-			} catch(Exception e) {
+    @Override
+    public void hide() {
+        logger.info("Hiding Splash-screen");
 
-			}
-		}
+        if (System.currentTimeMillis() - startTime < MIN_TIME) {
+            try {
+                Thread.sleep(MIN_TIME - (System.currentTimeMillis() - startTime));
+            } catch (Exception e) {
+            }
+        }
 
-		SwingUtilities.doInEDTNow(new Runnable() {
-			@Override
-			public void run() {
-				splashScreenDialog.setVisible(false);
-				splashScreenDialog.dispose();
-			}
-		});
-	}
+        SwingUtilities.doInEDTNow(new Runnable() {
 
-	/**
-	 * The actual splash screen dialog, that shows the image, the message
-	 * and dots following it to show the user that the program is loading.
-	 */
-	private class SplashScreenDialog extends JDialog {
+            @Override
+            public void run() {
+                splashScreenDialog.setVisible(false);
+                splashScreenDialog.dispose();
+            }
+        });
+    }
 
-		private static final long serialVersionUID = 7388884935911211935L;
+    /**
+     * The actual splash screen dialog, that shows the image, the message and
+     * dots following it to show the user that the program is loading.
+     */
+    private class SplashScreenDialog extends JDialog {
 
-		/**
-		 * The number of dots to be drawn at the end of the message
-		 */
-		private int status = 0;
+        private static final long serialVersionUID = 7388884935911211935L;
 
-		/**
-		 * Timer used to show the dots at the end of the message
-		 */
-		private javax.swing.Timer timer = new javax.swing.Timer(400, new ActionListener() {
-	        public void actionPerformed(ActionEvent e) {
-	            repaint();
-	        }
-	    });
+        /**
+         * The panel that actually displays stuff
+         */
+        private JPanel splashPanel;
+        /**
+         * The number of dots to be drawn at the end of the message
+         */
+        private int status = 0;
+        /**
+         * Timer used to show the dots at the end of the message
+         */
+        private javax.swing.Timer timer;
+        
+        /** 
+         * Enables double-buffering by default
+         */
+        public SplashScreenDialog() {
+            super.getRootPane().setDoubleBuffered(true);
+            
+            splashPanel = new JPanel() {
+                @Override
+                public void paintComponent(Graphics g) {
+                    g.drawImage(image, 0, 0, null);
+                    String message = Messages.splash_screen_loading_message + " ";
+                    for (int i = 0; i < status; i++) {
+                        message += ".";
+                    }
+                    g.drawString(message, 100, 265);
+                    status = ++status % 5;
+                    timer.start();
+                }                        
+            };
+            add(splashPanel);
 
-		@Override
-		public void paint(Graphics g) {
-			super.paint(g);
-			g.drawImage(image, 0, 0, null);
-			String message = Messages.splash_screen_loading_message + " ";
-			for (int i = 0; i < status; i++)
-				message += ".";
-			g.drawString(message, 100, 265);
-			status = ++status % 5;
-			timer.start();
-		}
-
-	}
-
+            timer = new javax.swing.Timer(400, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    splashPanel.repaint();
+                }
+            });
+        }               
+    }
 }
