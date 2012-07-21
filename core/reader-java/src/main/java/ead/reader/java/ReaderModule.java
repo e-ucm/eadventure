1a295c60b1d7c95eb6deb538a07ca70f32035810
anserran <angelserranolaguna@gmail.com>
2012-07-21 13:46:49 +0200

    Added and modified poms for the new projects. (TODO: Install fails in the FirstAidDemo)

--- core/reader-java/src/main/java/ead/reader/java/ReaderModule.java
+++ core/reader-java/src/main/java/ead/reader/java/ReaderModule.java
@@ -35,47 +35,17 @@
  *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package ead.importer;
+package ead.reader.java;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.AbstractModule;
 
-import ead.importer.EAdventure1XImporter.ImporterProgressListener;
-import ead.reader.java.ReaderModule;
-import ead.tools.java.JavaToolsModule;
+import ead.reader.StringFileHandler;
 
-public class Main {
+public class ReaderModule extends AbstractModule {
 
-	public static void main(String[] args) {
-		String source = args[0];
-		String destiny = args[1];
-		String format = "none";
-		if (args.length > 3) {
-			if (args[2].equals("-f")) {
-				if (args[3].equals("none") || args[3].equals("zip")
-						|| args[3].equals("ead")) {
-					format = args[3];
-				} else {
-					System.out.println("Unknown format " + args[3]);
-				}
-			} else {
-				System.out.println("Unknown option " + args[2]);
-			}
-		}
-		
-		Injector injector = Guice.createInjector(new ImporterModule( ), new JavaToolsModule(), new ReaderModule());
-		EAdventure1XImporter importer = injector.getInstance(EAdventure1XImporter.class);
-	
-		importer.addProgressListener(new ImporterProgressListener( ){
-
-			@Override
-			public void update(int progress, String text) {
-				System.out.println(progress + "% " + text);
-			}
-			
-		});
-		
-		importer.importGame(source, destiny, format);
+	@Override
+	protected void configure() {
+		bind(StringFileHandler.class).to(DefaultStringFileHandler.class);
 	}
 
 }
