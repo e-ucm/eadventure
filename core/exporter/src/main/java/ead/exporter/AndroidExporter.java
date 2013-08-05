19d9c08ab43aa4b5317e2bb3381df85945d1687a
anserran <angelserranolaguna@gmail.com>
2013-08-05 09:01:38 +0200

    Some work in android export

---
+++ core/exporter/src/main/java/ead/exporter/AndroidExporter.java
@@ -0,0 +1,59 @@
+/**
+ * eAdventure (formerly <e-Adventure> and <e-Game>) is a research project of the
+ *    <e-UCM> research group.
+ *
+ *    Copyright 2005-2010 <e-UCM> research group.
+ *
+ *    You can access a list of all the contributors to eAdventure at:
+ *          http://e-adventure.e-ucm.es/contributors
+ *
+ *    <e-UCM> is a research group of the Department of Software Engineering
+ *          and Artificial Intelligence at the Complutense University of Madrid
+ *          (School of Computer Science).
+ *
+ *          C Profesor Jose Garcia Santesmases sn,
+ *          28040 Madrid (Madrid), Spain.
+ *
+ *          For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *          <http://www.e-ucm.es>
+ *
+ * ****************************************************************************
+ *
+ *  This file is part of eAdventure, version 2.0
+ *
+ *      eAdventure is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      eAdventure is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with eAdventure.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ead.exporter;
+
+import java.util.Properties;
+
+public class AndroidExporter {
+
+	public void export(Properties properties) {
+
+		// Create temp file
+		// Copy build.xml
+		// Create src
+		// Create res/drawable, add icon
+		// Create manifest -> change title
+		// Create project.properties
+		// Find ead.engine-android.jar
+		// Find native libs
+		// Copy engine assets to temp
+		// Copy game assets to temp
+		// Launch ant
+
+	}
+}
