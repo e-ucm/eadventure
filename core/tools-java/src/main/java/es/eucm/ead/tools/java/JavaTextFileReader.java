716ab8abf8f8e15ba70af9233eab060472272834
anserran <angelserranolaguna@gmail.com>
2013-08-19 18:55:55 +0200

    Added a java text file reader

---
+++ core/tools-java/src/main/java/es/eucm/ead/tools/java/JavaTextFileReader.java
@@ -0,0 +1,34 @@
+package es.eucm.ead.tools.java;
+
+import es.eucm.ead.tools.TextFileReader;
+
+import java.io.*;
+
+public class JavaTextFileReader implements TextFileReader {
+	@Override
+	public String read(String fileName) {
+        if ( fileName.startsWith("@")){
+            fileName = fileName.substring(1);
+        }
+        String text = "";
+		File f = new File(fileName);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(f));
+			String line;
+            while ((line = reader.readLine())!= null){
+                text += line + System.getProperty("line.separator");
+            }
+		} catch (IOException e) {
+            return null;
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		return text;
+	}
+}
